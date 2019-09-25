b7fa6e5fa6688fe29c34e29eabcd7f5620311946
Patrik Svoboda <svob.pata@gmail.com>
2019-09-25 20:05:13 +0200

    initial commit

--- Sources/URLImage/URLImage.swift
+++ Sources/URLImage/URLImage.swift
@@ -1,3 +1,41 @@
-struct URLImage {
-    var text = "Hello, World!"
+//
+//  URLImage.swift
+//
+//  Created by Patrik Svoboda on 30/08/2019.
+//  Copyright © 2019 Patrik Svoboda. All rights reserved.
+//
+
+import SwiftUI
+import ImageIOSwift
+import ImageIOSwiftUI
+
+struct URLImage: View {
+    // MARK: Properties
+    private var urlString: String
+    private var url: URL
+    private var defaultURL: String = "https://google.com"
+    // MARK: Functions
+    init(_ urlString: String) {
+        self.urlString = urlString
+        self.url = URL(string: self.defaultURL)!
+        self.url = self.guardURL(urlString)
+    }
+    private func guardURL(_ string: String) -> URL! {
+        guard let url = URL(string: string) else {
+            return URL(string: self.defaultURL)!
+        }
+        return url
+    }
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            // MARK: Placeholder
+            Image("placeholder")
+                .resizable()
+                .renderingMode(.original)
+            // MARK: The image
+            URLImageSourceView(url)
+                .aspectRatio(contentMode: .fill)
+        }
+    }
 }
