b982205e7ae5b47efb06e4a469ed41938da6db6a
Patrik Svoboda <svob.pata@gmail.com>
2019-09-25 20:36:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "URLImage",
-            dependencies: ["ImageIOSwift"]
+            dependencies: ["ImageIOSwift", "ImageIOSwiftUI"]
         ),
         .testTarget(
             name: "URLImageTests",
